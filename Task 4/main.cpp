ddabd707eeb06303d0c81168fa680d8b2073aa98
Yaseen El-Olemy <yaseen.elolemy@gmail.com>
2024-10-31 17:17:28 +0300

    menu prototype and 2nd operation

--- Task 4/main.cpp
+++ Task 4/main.cpp
@@ -6,29 +6,57 @@
 #include "vole.h"
 using namespace std;
 
-int hex_to_dec(string hex)      //frequently used to convert from hex to decimal
+void OP2(int target, string pattern, Machine& mac)      //operation 2
 {
-    int y;
-    std::stringstream stream;
-
-
-    stream << hex;
-    stream >> std::hex >> y;
-     return y;
-
+    mac.SetRegisterValue(target, pattern);
 }
 
-int main() {
+void executor(Machine& mac, int inc)
+{
+    vector<Memory>& memory = mac.getMemory();
+    for(int i = 0; i % 2 == 0 || i < inc; ++i)
+    {
+        if(memory[i].GetValue()[0] == '2')
+        {
+            int tar = memory[i].GetValue()[1] - '0';
+            OP2(tar, memory[i+1].GetValue(), mac);
+        }
+    }
+}
 
-    ifstream inputfile;
-    Machine machine;
-    int spaces=0;
-    string filename, contents, reg_name;
+
+
+void displayMemory(Machine& mac)
+{
+    vector<Memory>& memory = mac.getMemory(); //get the memory
+
+    cout << "Memory: " <<endl;
+
+    for (int i = 0; i < memory.size(); i++) { //test loop to print the memory
+        cout << memory[i].GetValue() << " ";
+    }
+    cout << endl;
+}
+void displayRegister(Machine& mac)
+{
+    string reg_name;
     reg_name = "0123456789ABCDEF";
+    vector<Register>& registers = mac.getRegisters(); //get the registers
+    for (int i = 0; i < registers.size(); i++) { //test loop to print the registers
+        cout<<"register[0x" << reg_name[i] << "]: " << '\t';
+        cout << registers[i].getvalue() << endl;
+    }
+    cout << "---------------------------------------------------------" <<endl;
+}
+
+int loadFile(string filename, Machine& machine)
+{
+    ifstream inputfile;
+    int inc = 0;
+    string contents;
+    int spaces;
     string hex1 = "";
-    vector<string> lines;
-    cout<<"Please enter the file name: ";    //elmafroud n7ot 7etet el input di f class esmo instructions w n3ml function esmo readfile w ne3mlo declare gowa elmachine bs fokak delwa2ty aham 7aga el operations
-    getline(cin, filename);  //fa shof lw hate3raf te3ml 7aga fel operations el awl w nseb el instructions ba3den
+    vector <string> lines;
     if(!"E:\\FCI\\Object Oriented Programming\\Task 4\\"+inputfile.is_open()) {
         cout<<"File does not exist"<<endl;
     }
@@ -59,13 +87,77 @@ int main() {
             if(counter==2) {
                 counter=0;
                 machine.SetMemoryValue(index,separtor);
+                ++inc;
                 index++;
                 separtor="";
             }
         }
     }
+    return inc;
+}
+int hex_to_dec(string hex)      //frequently used to convert from hex to decimal
+{
+    int y;
+    std::stringstream stream;
+    stream << hex;
+    stream >> std::hex >> y;
+     return y;
 
-    vector<Memory>& memory = machine.getMemory(); //get the memory
+}
+
+
+
+int main() {
+    int inc=0;
+    ifstream inputfile;
+    ;
+    int spaces=0;
+    string filename, contents;
+    Machine machine;
+    int choice=1000;
+    string hex1 = "";
+    vector<string> lines;
+    while(choice !=0)
+    {
+        cout<<"[1] - Load new program from file" << endl << "[2] - Execute Program" << endl << "[3] - Display Status" << endl << "[0] - Exit Program"<<endl << "-> ";
+
+        cin >> choice;
+        switch (choice)
+        {
+            case 1:
+
+                cout << "Enter file name: ";
+                getline(cin, filename);
+                getline(cin, filename);
+                inc = loadFile( filename, machine);
+                break;
+            case 2:
+                executor(machine, inc);
+                break;
+            case 3:
+                displayMemory(machine);
+                displayRegister(machine);
+                break;
+
+        }
+
+
+    }
+
+    //cout<<"Please enter the file name: ";    //elmafroud n7ot 7etet el input di f class esmo instructions w n3ml function esmo readfile w ne3mlo declare gowa elmachine bs fokak delwa2ty aham 7aga el operations
+    //getline(cin, filename);  //fa shof lw hate3raf te3ml 7aga fel operations el awl w nseb el instructions ba3den
+
+    char command = ' ';
+    //for(int i =0; i % 2 == 0 && i < inc; ++i)
+    {
+
+    }
+
+
+
+    //Bellow the line is the final step which is print and displaying register and memory values
+
+    /*vector<Memory>& memory = machine.getMemory(); //get the memory
 
     cout << "Memory: " <<endl;
 
@@ -77,7 +169,9 @@ int main() {
     for (int i = 0; i < registers.size(); i++) { //test loop to print the registers
         cout<<"register[0x" << reg_name[i] << "]: " << '\t';
         cout << registers[i].getvalue() << endl;
-    }
+    }*/
 
     return 0;
 }
+
+
